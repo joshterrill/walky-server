6a1318c67594ae8be1b781aad4855725961d9798
Joshua Terrill <joshterrill.dev@gmail.com>
2015-08-27 00:34:59 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# walky
+a walky talky implementation using webrtc
+
+### to install
+git clone https://github.com/joshuaterrill/walky.git
+cd walky/
+npm install
+node server.js
---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require("express");
+var app = express();
+var http = require('http').Server(app);
+var port = process.env.PORT || 3000;
+
+var path = require("path");
+var randomstring = require("randomstring");
+
+app.set('views', path.join(__dirname, '/public'));
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+  var uuid = randomstring.generate(7);
+  //res.render('index', { uuid: randomstring.generate(7) });
+  res.redirect("/" + uuid);
+});
+
+app.get('/:roomId', function(req, res) {
+  var roomId = req.params.roomId;
+  res.render('index', { uuid: roomId });
+}); 
+
+
+http.listen(port, function(){
+    console.log('listening on *:' + port);
+});
